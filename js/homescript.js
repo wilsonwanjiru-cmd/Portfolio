2051b55a7f1b3e80bd251f56066455a6a88ed1d5
Viraj Jain <harshal.jain.csai.20@ggits.net>
2024-08-13 01:59:24 +0530

    complete

---
+++ js/homescript.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let text = ["Harshal Jain", "a Full Stack Developer", "a mentor at GeeksForGeeks"];
+
+    let count = 0;
+    let index = 0;
+    let currentText = '';
+    let letter = '';
+
+    (function type() {
+        if (count == text.length) {
+            count = 0;
+        }
+
+        currentText = text[count];
+        letter = currentText.slice(0, ++index);
+        console.log(letter);
+
+        const h1 = document.querySelector('.home .left h1');
+        h1.textContent = letter;
+
+        if (letter.length == currentText.length) {
+            count++;
+            index = 0;
+            setTimeout(type, 2000);
+        }
+        else {
+            setTimeout(type, 100);
+        }
+
+    }())
+})
